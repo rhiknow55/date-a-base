5bb3a8e1da6c9564ad037005d07750710a9f66b2
r6m2b <r6m2b@cs.ubc.ca>
2019-06-04 14:26:29 -0700

    implemented: getting pw from file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // create a GET route
 app.get('/express_backend', (req, res) => {
 
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
 /*
@@ -30,15 +30,20 @@ app.get('/ryan_test', (req, res) => {
 //todo: everything related to mysql is better to be written in another file (to make server.js look cleaner)
 var mysql = require('mysql');
 
+// get password from file
+// file name must be "secret", no extension
+// it must be plaintext with only the password inside
+var fs = require('fs');
+var mysql_password = fs.readFileSync('secret', 'utf8');
+
 //note: a database named "date-a-base" need to be created before connection
 //todo: for security reason (since this would be an open source project), don't put password in the code,
 // instead, write it in a separate file
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  //password: secret.mysql_password
-  password: "MySqlRoot-0605",
-  database: "date-a-base"
+    host: "localhost",
+    user: "root",
+    password: mysql_password,
+    database: "date-a-base"
 });
 
 //create tables
@@ -66,17 +71,17 @@ var con = mysql.createConnection({
 
 //sample data insertion
 //all sql statements (except connection and table creation) should be in request handler methods
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  //var birthday = new Date('1988-02-10'); //todo: not working, null value inserted
-  //var horoscope = 1;//todo: e.g. userHoroscope = getHoroscope(birthday), use enum
-  var sql = "INSERT INTO users (userId, loginName, password, username, emailAddress, birthday, horoscope)" +
-      " VALUES (00011, 'jamieTestUser5', 'dateabase1', 'jamie test user', 'jamie@gmail.com', Date('1958-02-10'), 1)";
-  con.query(sql, function (err, result) {
+con.connect(function (err) {
     if (err) throw err;
-    console.log("1 record inserted");
-  });
+    console.log("Connected!");
+    //var birthday = new Date('1988-02-10'); //todo: not working, null value inserted
+    //var horoscope = 1;//todo: e.g. userHoroscope = getHoroscope(birthday), use enum
+    var sql = "INSERT INTO users (userId, loginName, password, username, emailAddress, birthday, horoscope)" +
+        " VALUES (00011, 'jamieTestUser5', 'dateabase1', 'jamie test user', 'jamie@gmail.com', Date('1958-02-10'), 1)";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
 });
 
 //con.end();//todo: we can't add this line here, error occurs, Error: Cannot enqueue Query after invoking quit.
