40e15ea8b253b2db3bf1897014c2e9a16d7c06ae
jamieWZ <weihongwang1119@gmail.com>
2019-06-17 20:51:57 -0700

    added packages

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -2,14 +2,15 @@ var connection = require('./connection.js');
 
 // '/user_data'
 exports.userData = function(req, res){
-    connection.query('SELECT username, horoscope, log FROM users WHERE userId = ?', req.query.userId,
+    connection.query('SELECT username, horoscope, log, baseId FROM users WHERE userId = ?', req.query.userId,
         function (err, rows, fields) {
             if (err) throw err;
 
             res.send({
                 "username": rows[0].username,
                 "horoscope": rows[0].horoscope,
-                "log": rows[0].log
+                "log": rows[0].log,
+                "baseId": rows[0].baseId,
             })
         })
 };
